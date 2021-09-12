4ad17490113088e8163cd6593b49158fc6bb8ade
Kiran Ajikumar <73101486+kiranajik@users.noreply.github.com>
2021-09-13 00:25:28 +0530

    second set done

--- 16q-postFixEvaluationStack.c
+++ 16q-postFixEvaluationStack.c
@@ -1,64 +1,71 @@
-#include<stdio.h>
-int stack[20];
-int top = -1;
-
-void push(int x)
+#include<stdio.h>     //standard input output functions
+#include<conio.h>        //console functions
+#include<string.h>       //string functions
+#define MAX 50              //max size defined
+int stack[MAX];             //a global stack
+char post[MAX];             //a global postfix stack
+int top=-1;                  //initializing top to -1
+void pushstack(int tmp);       //push function
+void evaluate(char c);          //calculate function
+void main()
 {
-    stack[++top] = x;
+   int i,l;
+   //clrscr();
+   printf("Insert a postfix notation :: ");
+   gets(post);                    //getting a postfix expression
+   l=strlen(post);               //string length
+   for(i=0;i<l;i++)
+   {
+      if(post[i]>='0' && post[i]<='9')
+      {
+          pushstack(i);             //if the element is a number push it
+      }
+      if(post[i]=='+' || post[i]=='-' || post[i]=='*' ||
+      post[i]=='/' || post[i]=='^')       //if element is an operator
+      {
+          evaluate(post[i]);             //pass it to the evaluate
+      }
+   }                      //print the result from the top
+   printf("\n\nResult :: %d",stack[top]);
+   getch();
 }
 
-int pop()
+void pushstack(int tmp)          //definiton for push
 {
-    return stack[top--];
+   top++;                              //incrementing top
+   stack[top]=(int)(post[tmp]-48);    //type casting the string to its integer value
 }
 
-int main()
+void evaluate(char c)       //evaluate function
 {
-    char exp[20];
-    char *e;
-    int n1,n2,n3,num;
-    printf("Enter the expression :: ");
-    scanf("%s",exp);
-    e = exp;
-    while(*e != '\0')
-    {
-        if(isdigit(*e))
-        {
-            num = *e - 48;
-            push(num);
-        }
-        else
-        {
-            n1 = pop();
-            n2 = pop();
-            switch(*e)
-            {
-            case '+':
-            {
-                n3 = n1 + n2;
-                break;
-            }
-            case '-':
-            {
-                n3 = n2 - n1;
-                break;
-            }
-            case '*':
-            {
-                n3 = n1 * n2;
-                break;
-            }
-            case '/':
-            {
-                n3 = n2 / n1;
-                break;
-            }
-            }
-            push(n3);
-        }
-        e++;
-    }
-    printf("\nThe result of expression %s  =  %d\n\n",exp,pop());
-    return 0;
+   int a,b,ans;        //variables used
+   a=stack[top];       //a takes the value stored in the top
+   stack[top]='\0';     //make the stack top NULL as its a string
+   top--;                //decrement top's value
+   b=stack[top];         //put the value at new top to b
+   stack[top]='\0';      //make it NULL
+   top--;                //decrement top
+   switch(c)     //check operator been passed to evaluate
+   {
+      case '+':          //addition
+          ans=b+a;
+          break;
+      case '-':           //subtraction
+          ans=b-a;
+          break;
+      case '*':            //multiplication
+          ans=b*a;
+          break;
+      case '/':           //division
+          ans=b/a;
+          break;
+      case '^':      //power
+          ans=b^a;
+          break;
+      default:
+          ans=0;      //else 0
+   }
+   top++;             //increment top
+   stack[top]=ans;        //store the answer at top
 }
 
